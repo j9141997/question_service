ea6ea3021f5fc2cabc13f163644cd5bd0114fb89
Junki Yoshida <j9141997@gmail.com>
2020-07-09 23:09:02 +0900

    set _docucment

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from "next/document";
+
+interface Props {
+  locale: "en-us" | "ja-JP"
+}
+
+export default class MyDocument extends Document<Props> {
+  render() {
+    return (
+      <Html lang={this.props.locale.split("-")[0]}>
+        <Head>
+          <meta name="viewport" content="width=device-width,height=device-height" key="viewport" />
+          <meta name="theme-color" content="#fff" key="themeColor" />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
