cb7c9f64dab91c1494f2f3c29a3d61303f89ed7a
j9141997 <j9141997@gmail.com>
2020-08-06 23:44:44 +0900

    add routes

--- src/components/molecule/HeaderItem.tsx
+++ src/components/molecule/HeaderItem.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
+
 import { Button } from '../atom/Button';
 
 export const HeaderItem = () => (
   <React.Fragment>
-    <Button color="inherit">
+    <Link href="/register">
       ログイン / 新規登録
+    </Link>
+    <Button color="inherit">
+      yoshidajunki
     </Button>
     <Button color="inherit">
       回答する
--- src/components/organism/header.tsx
+++ src/components/organism/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
