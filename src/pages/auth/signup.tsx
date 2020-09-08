35c0f2e4b3b7ad05cb924d25b46c102854493145
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2020-09-09 01:10:26 +0900

    update files

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
+import Router from 'next/router'
 import styled from 'styled-components'
 
 import AuthContainer from '../../components/organism/AuthContainer'
@@ -6,40 +7,59 @@ import Form from '../../components/organism/Form'
 import InputGroup from '../../components/molecule/InputGroup'
 import SubmitButton from '../../components/atom/SubmitButton'
 
+import auth from '../../utils/firebase'
+
+type valueProps = {
+  nickname: string
+  email: string
+  password: string
+}
+
+const Signup: FC = () => {
+  const [value, setValue] = useState<valueProps>({
+    nickname: '',
+    email: '',
+    password: '',
+  })
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user && Router.push('/')
+    })
+  }, [])
+
+  return (
+    <AuthContainer>
+      <Form action="hogehoge">
+        <RegisterTitle>Shitumonへようこそ！</RegisterTitle>
+        <InputGroup
+          labelName="ニックネーム"
+          name="nickname"
+          placeholder="Shitumonmon"
+          value={value.nickname}
+        />
+        <InputGroup
+          labelName="メールアドレス"
+          name="email"
+          placeholder="shitumon@shitumon.com"
+          value={value.email}
+        />
+        <InputGroup
+          labelName="パスワード"
+          name="password"
+          placeholder="6文字以上で入力してください"
+          type="password"
+          value={value.password}
+        />
+        <SubmitButton value="メールアドレスで登録" />
+      </Form>
+    </AuthContainer>
+  )
+}
+
 const RegisterTitle = styled.h1`
   font-size: 2.8rem;
   font-weight: 800;
 `
 
-const register: FC = () => (
-  <AuthContainer>
-    <Form action="hogehoge">
-      <RegisterTitle>Shitumonへようこそ！</RegisterTitle>
-      <InputGroup
-        labelName="ニックネーム"
-        name="nickname"
-        placeholder="Shitumonmon"
-      />
-      <InputGroup
-        labelName="メールアドレス"
-        name="email"
-        placeholder="shitumon@shitumon.com"
-      />
-      <InputGroup
-        labelName="パスワード"
-        name="password"
-        placeholder="6文字以上で入力してください"
-        type="password"
-      />
-      <InputGroup
-        labelName="パスワードの確認"
-        name="password_confirmation"
-        placeholder="6文字以上で入力してください"
-        type="password"
-      />
-      <SubmitButton value="メールアドレスで登録" />
-    </Form>
-  </AuthContainer>
-)
-
-export default register
+export default Signup
