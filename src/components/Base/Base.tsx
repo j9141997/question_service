bc756751c00db636e0b079276e777e6cec3661c7
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2020-09-16 23:44:17 +0900

    feat: init Base Component and update Panel Component

---
+++ src/components/Base/Base.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode, forwardRef } from react
+import styled, { css } from 'styled-components'
+
+type Props = {
+  children: ReactNode
+  radius?: 's' | 'm'
+  className?: string
+}
+
+export const Base = forwardRef<HTMLDivElement, Props>(
+  ({ radius = 'm', ...props }, ref) => {
+    const radiusMap = {
+      s: '6px',
+      m: '8px',
+    }
+    return <Wrapper radius={radiusMap[radius]} ref={ref} {...props} />
+  }
+)
+
+const Wrapper = styled.div<{ radius: string }>`
+  ${({ radius }) => {
+    return css`
+      border-radius: ${radius};
+      box-shadow: rgba(51, 51, 51, 0.3) 1px 1px 4px 0;
+      background-color: #fff;
+    `
+  }}
+`
