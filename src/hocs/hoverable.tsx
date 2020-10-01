89be6181fcbe50a9595a71765503d2b1cb2e5217
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2020-10-01 21:56:48 +0900

    feat: add type

--- src/hocs/hoverable.tsx
+++ src/hocs/hoverable.tsx
@@ -1,2 +1,10 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import { isMouseDevise, isTouchDevise } from '../libs/ua'
+
+export type ExternalProps = {
+  className?: string
+  onMouseEnter?: (e: MouseEvent) => void
+  onMouseLeave?: (e: MouseEvent) => void
+  onTouchStart?: (e: TouchEvent) => void
+  onTouchEnd?: (e: TouchEvent) => void
+}
